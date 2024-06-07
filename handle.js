d2481512c99feec71cee5735767cfd75ac0b6059
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-06-07 14:25:57 +0700

    api cao web

---
+++ handle.js
@@ -0,0 +1,4 @@
+let socket = io("http://localhost:4000/")
+$(document).ready(()=>{
+    alert("123")
+})
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = 4000;
 const app = express();
 const server = createServer(app);
 
+// Crawl
+const cheerio = require("cheerio")
+const request = require("request-promise")
+
 // static files
 const path = require("path");
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -12,19 +16,70 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "view"));
 
+
+// Crawl data with pagination
+app.use("/api/v1", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; 
+    const size = parseInt(req.query.size) || 4; 
+
+    const html = await request('https://123job.vn/tuyen-dung');
+    const $ = cheerio.load(html);
+    const jobList = [];
+
+    $(".job__list-item").each((index, element) => {
+      const job = $(element).find(".job__list-item-content .job__list-item-title").text().trim();
+      const company = $(element).find(".job__list-item-content .job__list-item-company").text().trim();
+      const address = $(element).find(".job__list-item-content .job__list-item-info .address").text().trim();
+      const salary = $(element).find('.job__list-item-info').find('.salary').text().trim();
+
+      jobList.push({
+        job: job,
+        company: company,
+        address: address,
+        salary: salary
+      });
+    });
+  
+
+        const totalItems = jobList.length;
+        const totalPages = Math.ceil(totalItems / size);
+        const startIndex = (page - 1) * size;
+        const endIndex = Math.min(startIndex + size, totalItems);
+    
+        const paginatedJobs = jobList.slice(startIndex, endIndex);
+    
+        res.status(200).json({
+          page: page,
+          size: size,
+          totalItems: totalItems,
+          totalPages: totalPages,
+          jobs: paginatedJobs
+        });
+      } catch (error) {
+        res.status(500).json("server invalid");
+      }
+    });
+
+
+
 // socket.io
 const { Server } = require("socket.io");
+const { match } = require("node:assert");
 const io = new Server(server);
 
 // listening to client
 const arrayUsers = [];
 
 io.on("connection", (socket) => {
+  
   console.log("co nguoi ket noi ", socket.id);
 
+ 
 
   // lang nghe ten nguoi dung
   socket.on("client-send-username", (data) => {
+
     console.log("data user gui ", data, arrayUsers.includes(data));
 
     // neu da ton tai
@@ -39,6 +94,23 @@ io.on("connection", (socket) => {
     }
   });
 
+
+  // join-room and name-room
+  socket.on("join-room",(data)=>{
+      socket.join("create-"+data)
+      console.log("day la 1 socket ", socket.adapter.rooms);
+      let mang = []
+      for (const [room, connections] of socket.adapter.rooms) {
+        mang.push(room)
+      }
+      socket.emit("server-all-rooms",mang) 
+
+      const name_room = `create-${data}`
+      socket.emit("server-name-room",name_room) 
+  })
+
+ 
+  
 
   // ngat ket noi
   socket.on("disconnect", () => {
